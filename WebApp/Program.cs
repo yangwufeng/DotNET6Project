8e849b89cfb5127d936eae75d2f2cff20f296b7c
yangwufeng <15773567547@163.con>
2022-06-06 10:27:05 +0800

    AddSignalR

--- WebApp/Program.cs
+++ WebApp/Program.cs
@@ -1,5 +1,6 @@
 using Common.Config;
 using Service;
+using WebApp.SignalRChat;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -7,6 +8,8 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.Configure<ConnectionStrings>(builder.Configuration.GetSection("Appsettings"));
 
+//处理事件同步
+builder.Services.AddSignalR();
 #endregion
 
 
@@ -34,5 +37,7 @@ app.UseAuthorization();
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.MapHub<ChatHub>("/chatHub");
 
 app.Run();
